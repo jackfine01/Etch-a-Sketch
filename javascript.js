c8276fe1291a01ce0903defd7fdb49e089ae9d6c
jack <jackfine01@gmail.com>
2024-12-12 16:42:34 -0500

    adding intial javascript for coloumn creation.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const container = document.createElement('div');
+container.className = "container";
+document.body.appendChild(container);
+
+    let col = document.createElement('div');
+    col.className = "column";
+    container.appendChild(col);
