19a39ab05bf66c498dae33c977a6261335303802
jack <jackfine01@gmail.com>
2024-12-12 16:59:24 -0500

    adding javascript nested for-loop to create rows and columns, adding css styling for visuals

--- javascript.js
+++ javascript.js
@@ -2,6 +2,13 @@ const container = document.createElement('div');
 container.className = "container";
 document.body.appendChild(container);
 
+for(let i = 0; i< 15; i++){
     let col = document.createElement('div');
     col.className = "column";
     container.appendChild(col);
+        for(let j = 0; j < 15; j++){
+            let row = document.createElement('div');
+            row.className = 'row';
+            col.appendChild(row);
+        };
+};
