30ac12e13e2be49e51543941de7c27e9cb3f1b46
jack <jackfine01@gmail.com>
2024-12-13 11:21:46 -0500

    added column adding/removal when reset button is pressed.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,38 @@
-let size = 0;
-
-function getSize() 
-    {
-        size = prompt("How many squares per side would you like?");
-    };
+// let size = 16;
 
 let resetButton = document.getElementById('reset');
 
-resetButton.addEventListener('click', () => {size = prompt("How many squares per side would you like?");});
+resetButton.addEventListener('click', 
+    () => 
+        {
+            let size = prompt("How many squares per side would you like?");
+            //Delete Old Columns/Rows
+            let columnsToDelete = document.querySelectorAll('.column');
+
+            columnsToDelete.forEach(column=> {
+                column.remove();
+                }
+            )
+            // Add New Columns/Rows
+            for(let i = 0; i< size; i++){
+                let col = document.createElement('div');
+                col.className = "column";
+                container.appendChild(col);
+                    for(let j = 0; j < size; j++){
+                        let row = document.createElement('div');
+                        row.className = 'row';
+                        col.appendChild(row);            
+                    };
+                    const rows = document.querySelectorAll('.row');
+            
+                    rows.forEach(row=> {
+                        row.addEventListener('mouseenter', () => {
+                            row.style.backgroundColor = 'black';
+                        })
+                    })
+            };
+        }
+    );
 
 
 
